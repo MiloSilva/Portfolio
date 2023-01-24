8008ced48a1d6c7c91ab2ad7f5ecc15605cb8bd0
MiloSilva <jcsilvaroldan@gmail.com>
2023-01-23 23:12:32 -0500

    feat: added basic content to the landing site

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Navbar } from '@components';
+import { Landing } from '@pages';
 
 const root = document.getElementById('root');
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+    children: [],
+  },
+]);
 
 if (root === null) {
   console.error('No root found in index.html, please contact an admin');
 } else {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <Navbar />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,107 @@
+export const Landing = () => {
+  return (
+    <>
+      <section id="Introduction">
+        <h1>Juan Camilo Silva</h1>
+        <h2>FullStack Developer</h2>
+        <h3>Seasoning the Web</h3>
+        <p>
+          Innovative, detail focused, passionate, optimization nerd, these are
+          only a few words that have been used to describe me as a web developer
+          with a knack for bringing projects from simple ideas to working
+          products. Love for learning, experimenting and providing unique
+          solutions to unique problems and giving a new point of view to
+          existing projects. Proud of my coding and people skills, great at
+          troubleshooting and bug hunting, love for attention to detail and
+          viewing products from the user's point of view.
+        </p>
+      </section>
+      <section id="skills">
+        <h1>Tecnical Skills</h1>
+        <h4>HTML 100%</h4>
+        <h4>CSS3 100%</h4>
+        <h4>Typescript 100%</h4>
+        <h4>React 100%</h4>
+        <h4>REST APIs 100%</h4>
+        <h4>SQL & NoSQL 80%</h4>
+        <h4>Agile Technologies 80%</h4>
+      </section>
+      <section id="Experience">
+        <section>
+          <h1>Full Stack Developer</h1>
+          <h2>Kiwi Campus</h2>
+          <h6>September 2021 - Present</h6>
+          <ul>
+            <li>
+              Update and maintain the landing page with updated information and
+              new visuals provided by the design team of the company.
+            </li>
+            <li>
+              Brought to life a react component library from a document of
+              designs in search of unifying internal tools and ease the
+              development process
+            </li>
+            <li>
+              Developed a hub for various internal tools allowing for updating,
+              viewing and analyzing information gathered from the ongoing
+              operation.
+            </li>
+            <li>
+              Brought to life a new web application allowing for livestreaming
+              and monitoring of the bots.
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h1>Full Stack Developer</h1>
+          <h2>National University of Colombia</h2>
+          <h6>January 2020 - June 2021</h6>
+          <ul>
+            <li>
+              Led a group of developers in creating a new web application
+              hosting course literature, videos and exams.
+            </li>
+            <li>
+              Containerized the application and hosted it in AWS allowing for a
+              scalable infrastructure and minimal downtime.
+            </li>
+            <li>Provide server solutions for managing software licenses</li>
+          </ul>
+        </section>
+      </section>
+      <section id="Education">
+        <h1>Bachelor's degree in Software Engineering</h1>
+        <h2>Politecnico Gran Colombiano</h2>
+        <h6>2023 - Present</h6>
+      </section>
+      <section id="footer">
+        <h1>Hire me, I'm desperate</h1>
+        <br />
+        <h3>
+          <a href="mailto:jcsilvaroldan@gmail.com">jcsilvaroldan@gmail.com</a>
+        </h3>
+        <br />
+        <h3>
+          <a href="tel:+573113063382">+57 3113063382</a>
+        </h3>
+        <h3>
+          <a href="tel:+16092564623">+1 (609)256-4623</a>
+        </h3>
+        <br />
+        <h4>Trenton, New Jersey</h4>
+        <h4>Medellin, Colombia</h4>
+        <br />
+        <h3>
+          <a href="https://www.linkedin.com/in/miloflowers/">LinkedIn</a>
+        </h3>
+        <h3>
+          <a href="https://github.com/MiloSilva">Github</a>
+        </h3>
+        <br />
+        <h4>
+          <a href="https://github.com/MiloSilva/Portfolio">Source Code</a>
+        </h4>
+      </section>
+    </>
+  );
+};
